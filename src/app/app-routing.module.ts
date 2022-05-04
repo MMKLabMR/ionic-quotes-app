d1826c402f725a01d25b90d4eee87bc290a323a9
jelicas <jelicastanojevicc@gmail.com>
2022-05-04 12:40:16 +0200

    Add theme, auth service and guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
   {
     path: 'quotes',
-    loadChildren: () => import('./quotes/quotes.module').then( m => m.QuotesPageModule)
+    loadChildren: () => import('./quotes/quotes.module').then( m => m.QuotesPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'fun-facts',
-    loadChildren: () => import('./fun-facts/fun-facts.module').then( m => m.FunFactsPageModule)
+    loadChildren: () => import('./fun-facts/fun-facts.module').then( m => m.FunFactsPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'log-in',
+    loadChildren: () => import('./auth/log-in/log-in.module').then( m => m.LogInPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
   },
   {
     path: '',
